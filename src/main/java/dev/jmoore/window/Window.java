5d214ac5a7b859addbc67f4214db1f434fe69364
tycrek <gpg+github@jmoore.dev>
2023-04-10 15:30:20 -0600

    feat (ui): set up basic JavaFX Scene

--- src/main/java/dev/jmoore/window/Window.java
+++ src/main/java/dev/jmoore/window/Window.java
@@ -2,6 +2,13 @@ package dev.jmoore.window;
 
 import dev.jmoore.Grogu;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import lombok.val;
 
 public class Window extends Application {
 
@@ -10,5 +17,19 @@ public class Window extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        //#region           UI setup
+
+        // Mouse position label
+        val mousePositionLabel = new Label("Mouse position: 0x0");
+        var gridPane = new GridPane();
+        // Row 0
+        gridPane.add(mousePositionLabel, 0, 0);
+
+        StackPane ROOT = new StackPane(gridPane);
+
+        // Primary scene
+        Scene scene = new Scene(ROOT, WIDTH, HEIGHT, true, SceneAntialiasing.BALANCED);
+        //#endregion
     }
 }
