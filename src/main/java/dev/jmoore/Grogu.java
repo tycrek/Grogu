a1b2a0b424c9a4746c104431852cf1bca670aa1e
tycrek <gpg+github@jmoore.dev>
2023-04-10 15:27:38 -0600

    feat (ui): create Grogu.java & window/Window.java

---
+++ src/main/java/dev/jmoore/Grogu.java
@@ -0,0 +1,10 @@
+package dev.jmoore;
+
+import dev.jmoore.window.Window;
+import javafx.application.Application;
+
+public class Grogu {
+    public static void main(String[] args) {
+        Application.launch(Window.class, args);
+    }
+}
---
+++ src/main/java/dev/jmoore/window/Window.java
@@ -0,0 +1,14 @@
+package dev.jmoore.window;
+
+import dev.jmoore.Grogu;
+import javafx.application.Application;
+
+public class Window extends Application {
+
+    final static int WIDTH = 800;
+    final static int HEIGHT = 600;
+
+    @Override
+    public void start(Stage stage) {
+    }
+}
