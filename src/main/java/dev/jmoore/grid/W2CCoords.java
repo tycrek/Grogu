c04d538f84ca25ebd11c1fce0f7c0352deefc37f
tycrek <gpg+github@jmoore.dev>
2023-04-10 17:24:11 -0600

    fix: double scale

--- src/main/java/dev/jmoore/grid/W2CCoords.java
+++ src/main/java/dev/jmoore/grid/W2CCoords.java
@@ -6,5 +6,5 @@ public record W2CCoords() {
     public static double
             width = Window.WIDTH, height = Window.HEIGHT,
             x = 0, y = 0,
-            xScale = 1, yScale = 1;
+            xScale = 2, yScale = 2;
 }
