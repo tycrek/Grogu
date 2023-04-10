81a1b626f735c9e3416cd33a2e3ebc81222acff3
tycrek <gpg+github@jmoore.dev>
2023-04-10 15:28:13 -0600

    feat (ui): create `window/SimpleAlert.java`, a simple alert system

---
+++ src/main/java/dev/jmoore/window/SimpleAlert.java
@@ -0,0 +1,23 @@
+package dev.jmoore.window;
+
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+
+public class SimpleAlert {
+
+    static void show(String title, String message) {
+        show(title, message, ButtonType.OK);
+    }
+
+    static void show(String title, String message, ButtonType buttonType) {
+        var dialogPane = new DialogPane();
+        var dialog = new Dialog<>();
+
+        dialogPane.setContentText(message);
+        dialogPane.getButtonTypes().add(buttonType);
+        dialog.setDialogPane(dialogPane);
+        dialog.setTitle(title);
+        dialog.show();
+    }
+}
