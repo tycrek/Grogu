748e73f1a513f30ab780d7d2fd46c48335b17f4b
tycrek <gpg+github@jmoore.dev>
2023-04-10 21:50:19 -0600

    fix: don't name variables "foo"; remove unnecessary rounding

--- src/main/java/dev/jmoore/grid/Window2Cartesian.java
+++ src/main/java/dev/jmoore/grid/Window2Cartesian.java
@@ -13,14 +13,9 @@ public class Window2Cartesian {
         double sy = W2CCoords.yScale / W2CCoords.height; // pixel-to-Cartesian scale factor for y-axis
 
         // Calculate Cartesian coordinates of mouse cursor
-        double fooX = W2CCoords.cartesianCenterX + (x - W2CCoords.width / 2) * sx;
-        double fooY = W2CCoords.cartesianCenterY - (y - W2CCoords.height / 2) * sy;
+        double cartesianX = W2CCoords.cartesianCenterX + (x - W2CCoords.width / 2) * sx;
+        double cartesianY = W2CCoords.cartesianCenterY - (y - W2CCoords.height / 2) * sy;
 
-        // Trim to 4 decimal places
-        final double ROUND = 10000.0;
-        fooX = Math.round(fooX * ROUND) / ROUND;
-        fooY = Math.round(fooY * ROUND) / ROUND;
-
-        return new double[]{fooX, fooY};
+        return new double[]{cartesianX, cartesianY};
     }
 }
